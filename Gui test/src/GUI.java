a415e386bbecad0cac3d6769674f8c9d5019f55e
mattsfra <96086769+mattsfra@users.noreply.github.com>
2022-01-05 16:22:44 +0200

    Add files via upload

---
+++ Gui test/src/GUI.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener {
+	int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	
+	public GUI() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton button = new JButton("button");
+		button.addActionListener(this);
+		label = new JLabel("Number of clicks: 0");
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("My GUI");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new GUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		count++;
+		label.setText("Number of clicks: " + count);
+	}
+	
+}
